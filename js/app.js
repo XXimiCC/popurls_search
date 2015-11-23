ee5976d09c31a8b8346c6241b4e07a2fb6646550
dez <andrey.hetmanenko@iqria.com>
2015-11-23 18:41:56 +0200

    add redirect to google when select autocompleate or press enter

--- js/app.js
+++ js/app.js
@@ -6,13 +6,24 @@ var CJ = {
 $(function () {
   var searchInput = $(".search-input");
 
+  $("form.search").submit(function(event){
+    event.preventDefault();
+    window.open("http://www.google.com/search?q=" + searchInput.val(), "_blank");
+  });
+
   searchInput.autocomplete({
     source: function (query, callback) {
       return CJ.Suggestion.getSuggestions(query.term).then(function (data) {
         return callback(data);
       });
-    },
-  })
+    }
+  });
+  searchInput.data("ui-autocomplete")._renderItem = function (ul, item) {
+    return $("<li>")
+      .attr("data-value", item.suggestion)
+      .append("<a target='_blank' href='http://www.google.com/search?q=" + item.suggestion + "'>" + item.suggestion + "</a>")
+      .appendTo(ul);
+  };
   
   new CJ.SearchDropDown({
     renderTo: ".slider",
@@ -24,7 +35,7 @@ $(function () {
       var swiper = new Swiper(item, {
         slidesPerView: "auto",
         loop: true,
-        //freeMode: true,
+        freeMode: true,
         centeredSlides: true,
         spaceBetween: 30
       });
